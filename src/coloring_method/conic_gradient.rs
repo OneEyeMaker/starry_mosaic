759d4af2121764859f35c56fb8b0256d11bd8b72
OneEyeMaker <oneeyemaker@gmail.com>
2022-06-01 19:07:19 +0300

    Added ConicGradient type

---
+++ src/coloring_method/conic_gradient.rs
@@ -0,0 +1,52 @@
+use std::f64::consts;
+
+use palette::{Gradient, Mix};
+
+use super::{ColoringMethod, Vector};
+
+#[derive(Clone, Debug)]
+pub struct ConicGradient<Color>
+where
+    Color: Mix<Scalar = f64> + Clone,
+{
+    gradient: Gradient<Color>,
+    center_point: Vector,
+    angle: f64,
+    smoothness: f64,
+}
+
+impl<Color> ConicGradient<Color>
+where
+    Color: Mix<Scalar = f64> + Clone,
+{
+    pub fn new(colors: &[(f64, Color)], center_point: Vector, angle: f64, smoothness: f64) -> Self {
+        let gradient = Gradient::with_domain(Vec::from(colors));
+        Self {
+            gradient,
+            center_point,
+            angle: angle % (2.0 * consts::PI),
+            smoothness: smoothness.clamp(0.0, 1.0),
+        }
+    }
+    #[inline(always)]
+    pub fn new_smooth(colors: &[(f64, Color)], center_point: Vector, angle: f64) -> Self {
+        Self::new(colors, center_point, angle, 1.0)
+    }
+    #[inline(always)]
+    pub fn new_step(colors: &[(f64, Color)], center_point: Vector, angle: f64) -> Self {
+        Self::new(colors, center_point, angle, 0.0)
+    }
+}
+
+impl<Color> ColoringMethod<Color> for ConicGradient<Color>
+where
+    Color: Mix<Scalar = f64> + Clone,
+{
+    fn interpolate(&self, point: &Vector, center_point: &Vector) -> Color {
+        let smoothed_point = center_point.interpolate(point, self.smoothness);
+        let point_vector = &smoothed_point - &self.center_point;
+        let angle = point_vector.y.atan2(point_vector.x) - self.angle;
+        let clamped_angle = (angle + 2.0 * consts::PI) % (2.0 * consts::PI);
+        self.gradient.get(clamped_angle / (2.0 * consts::PI))
+    }
+}
--- src/coloring_method/mod.rs
+++ src/coloring_method/mod.rs
@@ -9,8 +9,10 @@ where
     fn interpolate(&self, point: &Vector, center_point: &Vector) -> Color;
 }
 
+mod conic_gradient;
 mod linear_gradient;
 mod radial_gradient;
 
+pub use self::conic_gradient::ConicGradient;
 pub use self::linear_gradient::LinearGradient;
 pub use self::radial_gradient::RadialGradient;
