4adb08043ecbbcdb7f6621351bf75b69b71e2f48
OneEyeMaker <oneeyemaker@gmail.com>
2022-06-07 21:00:10 +0300

    Refactored MosaicBuilder type: now it uses MosaicShape trait

--- src/mosaic_builder.rs
+++ src/mosaic_builder.rs
@@ -1,27 +1,39 @@
 use std::cmp::Ordering;
-use std::f64::consts;
 
 use voronoice::{BoundingBox, Point, VoronoiBuilder};
 
-use super::{segment::Segment, starry_mosaic::StarryMosaic, vector::Vector};
+use super::{
+    mosaic_shape::{MosaicShape, PolygonalStar, RegularPolygon},
+    starry_mosaic::StarryMosaic,
+    vector::Vector,
+};
 
-#[derive(Clone, Debug)]
 pub struct MosaicBuilder {
+    shape: Box<dyn MosaicShape>,
     image_size: (u32, u32),
-    points_count: usize,
-    rotation_angle: f64,
     center_point: Vector,
+    rotation_angle: f64,
     scale: f64,
-    is_complex_pattern: bool,
 }
 
 impl MosaicBuilder {
-    pub fn set_image_size(&mut self, width: u32, height: u32) -> &mut Self {
-        self.image_size = (width.max(1), height.max(1));
+    pub fn set_regular_polygon_shape(&mut self, corners_count: usize) -> &mut Self {
+        self.shape = Box::new(RegularPolygon::new(corners_count));
         self
     }
-    pub fn set_points_count(&mut self, points_count: usize) -> &mut Self {
-        self.points_count = points_count.max(3);
+    pub fn set_polygonal_star_shape(&mut self, corners_count: usize) -> &mut Self {
+        self.shape = Box::new(PolygonalStar::new(corners_count));
+        self
+    }
+    pub fn set_shape<Shape>(&mut self, shape: Shape) -> &mut Self
+    where
+        Shape: MosaicShape + 'static,
+    {
+        self.shape = Box::new(shape);
+        self
+    }
+    pub fn set_image_size(&mut self, width: u32, height: u32) -> &mut Self {
+        self.image_size = (width.max(1), height.max(1));
         self
     }
     pub fn set_center_point(&mut self, center_point: Vector) -> &mut Self {
@@ -39,16 +51,8 @@ impl MosaicBuilder {
         self.scale = scale.max(0.001);
         self
     }
-    pub fn set_simple_pattern(&mut self) -> &mut Self {
-        self.is_complex_pattern = false;
-        self
-    }
-    pub fn set_complex_pattern(&mut self) -> &mut Self {
-        self.is_complex_pattern = true;
-        self
-    }
     pub fn build_starry(&self) -> Option<StarryMosaic> {
-        let points = self.build_key_points();
+        let points = self.build_shape();
         let image_size = (self.image_size.0 as f64, self.image_size.1 as f64);
         let center = Point {
             x: image_size.0 / 2.0,
@@ -63,105 +67,30 @@ impl MosaicBuilder {
             None => None,
         }
     }
-    fn calculate_polygon_points(
-        &self,
-        additional_rotation: f64,
-        additional_scale: f64,
-    ) -> Vec<Vector> {
-        let radius =
-            self.image_size.0.min(self.image_size.1) as f64 * self.scale * additional_scale;
-        let mut points = Vec::new();
-        for index in 0..self.points_count {
-            let angle = consts::PI / self.points_count as f64
-                * (2 * index + 1 - self.points_count % 2) as f64
-                - consts::FRAC_PI_2
-                + additional_rotation;
-            points.push(Vector::new(
-                self.center_point.x + radius * angle.cos(),
-                self.center_point.y + radius * angle.sin(),
-            ));
-        }
-        points
-    }
-    fn create_simple_pattern_segments(&self, points: &Vec<Vector>) -> Vec<Segment> {
-        let points_count = points.len();
-        let mut segments = Vec::with_capacity(points_count * (points_count - 1) / 2);
-        for start_index in 0..points_count - 1 {
-            for end_index in start_index..points_count {
-                segments.push(Segment::new(
-                    points[start_index].clone(),
-                    points[end_index].clone(),
-                ));
-            }
-        }
-        segments
-    }
-    fn create_complex_pattern_segments(
-        &self,
-        outer_points: &Vec<Vector>,
-        inner_points: &Vec<Vector>,
-    ) -> Vec<Segment> {
-        debug_assert!(outer_points.len() == inner_points.len());
-        let points_count = outer_points.len();
-        let mut segments = Vec::new();
-        for start_index in 0..points_count {
-            let end_index = (start_index + 2) % points_count;
-            segments.push(Segment::new(
-                outer_points[start_index].clone(),
-                outer_points[end_index].clone(),
-            ));
-        }
-        for start_index in 0..points_count {
-            for end_index in start_index + 2..start_index + points_count - 2 {
-                let end_index = end_index % points_count;
-                segments.push(Segment::new(
-                    outer_points[start_index].clone(),
-                    inner_points[end_index].clone(),
-                ));
-            }
-        }
-        segments
-    }
-    fn intersect_segments(&self, segments: &Vec<Segment>, points: &mut Vec<Vector>) {
-        for (index, first_segment) in segments.iter().enumerate() {
-            for second_segment in segments[index + 1..].iter() {
-                if let Some(point) = first_segment.intersect(second_segment) {
-                    points.push(point);
-                }
-            }
-        }
-    }
-    fn build_key_points(&self) -> Vec<Point> {
-        let mut points = self.calculate_polygon_points(0.0, 1.0);
-        let segments = if self.is_complex_pattern {
-            let points_count = self.points_count as f64;
-            let additional_rotation = consts::PI / points_count;
-            let additional_scale = (consts::PI * (points_count * 0.5 - 2.0) / points_count).sin()
-                / (consts::FRAC_PI_2 / points_count * (points_count - 2.0)).sin();
-            let mut inner_points =
-                self.calculate_polygon_points(additional_rotation, additional_scale);
-            let segments = self.create_complex_pattern_segments(&points, &inner_points);
-            points.append(&mut inner_points);
-            segments
-        } else {
-            self.create_simple_pattern_segments(&points)
-        };
-        self.intersect_segments(&segments, &mut points);
-        points.sort_by(|left, right| left.partial_cmp(right).unwrap_or(Ordering::Equal));
-        points.dedup();
-        points.iter().map(|point| point.into()).collect()
+    fn build_shape(&self) -> Vec<Point> {
+        let mut initial_points = self.shape.set_up_points(
+            self.image_size,
+            self.center_point.clone(),
+            self.rotation_angle,
+            self.scale,
+        );
+        let shape_segments = self.shape.connect_points(&initial_points);
+        let mut shape_points = self.shape.intersect_segments(&shape_segments);
+        shape_points.append(&mut initial_points);
+        shape_points.sort_by(|left, right| left.partial_cmp(right).unwrap_or(Ordering::Equal));
+        shape_points.dedup();
+        shape_points.iter().map(|point| point.into()).collect()
     }
 }
 
 impl Default for MosaicBuilder {
     fn default() -> Self {
         Self {
+            shape: Box::new(RegularPolygon::default()),
             image_size: (400, 400),
-            points_count: 8,
-            rotation_angle: 0.0,
             center_point: Vector::new(200.0, 200.0),
+            rotation_angle: 0.0,
             scale: 0.75,
-            is_complex_pattern: false,
         }
     }
 }
