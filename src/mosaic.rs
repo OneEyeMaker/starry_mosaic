ebaf3de3c0562c7b27704f29d5bf4df6f327ca3f
OneEyeMaker <oneeyemaker@gmail.com>
2022-06-09 19:12:10 +0300

    Added functions to Mosaic trait for all gradient variants

--- src/mosaic.rs
+++ src/mosaic.rs
@@ -28,6 +28,30 @@ pub trait Mosaic {
             smoothness,
         ))
     }
+    fn draw_linear_smooth_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        start_point: Vector,
+        end_point: Vector,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(LinearGradient::new_smooth(gradient, start_point, end_point))
+    }
+    fn draw_linear_step_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        start_point: Vector,
+        end_point: Vector,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(LinearGradient::new_step(gradient, start_point, end_point))
+    }
     fn draw_radial_gradient<Color, ColorGradient>(
         &self,
         gradient: ColorGradient,
@@ -50,7 +74,47 @@ pub trait Mosaic {
             smoothness,
         ))
     }
-    fn draw_simple_radial_gradient<Color, ColorGradient>(
+    fn draw_radial_smooth_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        inner_center: Vector,
+        inner_radius: f64,
+        outer_center: Vector,
+        outer_radius: f64,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(RadialGradient::new_smooth(
+            gradient,
+            inner_center,
+            inner_radius,
+            outer_center,
+            outer_radius,
+        ))
+    }
+    fn draw_radial_step_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        inner_center: Vector,
+        inner_radius: f64,
+        outer_center: Vector,
+        outer_radius: f64,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(RadialGradient::new_step(
+            gradient,
+            inner_center,
+            inner_radius,
+            outer_center,
+            outer_radius,
+        ))
+    }
+    fn draw_radial_simple_gradient<Color, ColorGradient>(
         &self,
         gradient: ColorGradient,
         center: Vector,
@@ -64,6 +128,30 @@ pub trait Mosaic {
         self.draw(RadialGradient::new_simple(
             gradient, center, radius, smoothness,
         ))
+    }
+    fn draw_radial_simple_smooth_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        center: Vector,
+        radius: f64,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(RadialGradient::new_simple_smooth(gradient, center, radius))
+    }
+    fn draw_radial_simple_step_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        center: Vector,
+        radius: f64,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(RadialGradient::new_simple_step(gradient, center, radius))
     }
     fn draw_conic_gradient<Color, ColorGradient>(
         &self,
@@ -82,6 +170,30 @@ pub trait Mosaic {
             angle,
             smoothness,
         ))
+    }
+    fn draw_conic_smooth_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        center_point: Vector,
+        angle: f64,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(ConicGradient::new_smooth(gradient, center_point, angle))
+    }
+    fn draw_conic_step_gradient<Color, ColorGradient>(
+        &self,
+        gradient: ColorGradient,
+        center_point: Vector,
+        angle: f64,
+    ) -> RgbImage
+    where
+        Color: IntoColor<LinSrgb<f64>> + Mix<Scalar = f64> + Shade<Scalar = f64> + Clone,
+        ColorGradient: Into<Gradient<Color>>,
+    {
+        self.draw(ConicGradient::new_step(gradient, center_point, angle))
     }
     fn draw<Color, Method>(&self, coloring_method: Method) -> RgbImage
     where
