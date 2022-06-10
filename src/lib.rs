6d11fd35e54924f78444547f907ee69bfb7cbee9
OneEyeMaker <oneeyemaker@gmail.com>
2022-06-10 19:59:28 +0300

    Formed public API of the library

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-mod coloring_method;
-mod mosaic;
-mod mosaic_builder;
-mod mosaic_shape;
-mod segment;
-mod starry_mosaic;
 mod utility;
+
 mod vector;
+pub use self::vector::Vector;
+
+mod segment;
+pub use self::segment::Segment;
+
+pub mod coloring_method;
+
+pub mod mosaic_shape;
+
+mod mosaic;
+pub use self::mosaic::Mosaic;
+
+mod mosaic_builder;
+pub use self::mosaic_builder::MosaicBuilder;
+
+mod starry_mosaic;
+pub use self::starry_mosaic::StarryMosaic;
