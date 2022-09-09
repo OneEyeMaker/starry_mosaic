bab7fb08d996c201ddeefb9dfbfa8bde313e71eb
OneEyeMaker <oneeyemaker@gmail.com>
2022-09-09 13:33:01 +0300

    Renamed methods of TiltedGrid type for better readability
    Separated set method into a pair of set methods to match get accessors

--- src/mosaic_shape/tilted_grid.rs
+++ src/mosaic_shape/tilted_grid.rs
@@ -7,25 +7,29 @@ pub struct TiltedGrid {
 }
 
 impl TiltedGrid {
-    pub fn new(horizontal_cells_count: u32, vertical_cells_count: u32) -> Self {
+    pub fn new(rows_count: u32, columns_count: u32) -> Self {
         Self {
-            cells_count: (horizontal_cells_count.max(1), vertical_cells_count.max(1)),
+            cells_count: (rows_count.max(1), columns_count.max(1)),
             tilt_factor: Vector::default(),
         }
     }
 
     #[inline(always)]
-    pub fn horizontal_cells_count(&self) -> u32 {
+    pub fn rows_count(&self) -> u32 {
         self.cells_count.0
     }
 
     #[inline(always)]
-    pub fn vertical_cells_count(&self) -> u32 {
+    pub fn columns_count(&self) -> u32 {
         self.cells_count.1
     }
 
-    pub fn set_cells_count(&mut self, horizontal_cells_count: u32, vertical_cells_count: u32) {
-        self.cells_count = (horizontal_cells_count.max(1), vertical_cells_count.max(1));
+    pub fn set_rows_count(&mut self, rows_count: u32) {
+        self.cells_count.0 = rows_count.max(1);
+    }
+
+    pub fn set_columns_count(&mut self, columns_count: u32) {
+        self.cells_count.1 = columns_count.max(1);
     }
 
     #[inline(always)]
@@ -63,32 +67,22 @@ impl MosaicShape for TiltedGrid {
     ) -> Vec<Vector> {
         let (scaled_image_width, scaled_image_height) =
             (image_size.0 as f64 * scale, image_size.1 as f64 * scale);
+        let (rows_count, columns_count) = self.cells_count;
         let (horizontal_step_size, vertical_step_size) = (
-            scaled_image_width / self.cells_count.0 as f64,
-            scaled_image_height / self.cells_count.1 as f64,
+            scaled_image_width / columns_count as f64,
+            scaled_image_height / rows_count as f64,
         );
         let step_size = horizontal_step_size.min(vertical_step_size);
         let (horizontal_half_size, vertical_half_size) = (
-            step_size * self.cells_count.0 as f64 * 0.5,
-            step_size * self.cells_count.1 as f64 * 0.5,
+            step_size * columns_count as f64 * 0.5,
+            step_size * rows_count as f64 * 0.5,
         );
         let mut points = vec![];
         points.push(Vector::new(-horizontal_half_size, -vertical_half_size));
         points.push(Vector::new(-horizontal_half_size, vertical_half_size));
         points.push(Vector::new(horizontal_half_size, -vertical_half_size));
         points.push(Vector::new(horizontal_half_size, vertical_half_size));
-        for index in 1..self.cells_count.0 {
-            let index = index as f64;
-            points.push(Vector::new(
-                -horizontal_half_size + step_size * index,
-                -vertical_half_size,
-            ));
-            points.push(Vector::new(
-                -horizontal_half_size + step_size * index,
-                vertical_half_size,
-            ));
-        }
-        for index in 1..self.cells_count.1 {
+        for index in 1..rows_count {
             let index = index as f64;
             points.push(Vector::new(
                 -horizontal_half_size,
@@ -97,6 +91,17 @@ impl MosaicShape for TiltedGrid {
             points.push(Vector::new(
                 horizontal_half_size,
                 -vertical_half_size + step_size * index,
+            ));
+        }
+        for index in 1..columns_count {
+            let index = index as f64;
+            points.push(Vector::new(
+                -horizontal_half_size + step_size * index,
+                -vertical_half_size,
+            ));
+            points.push(Vector::new(
+                -horizontal_half_size + step_size * index,
+                vertical_half_size,
             ));
         }
         points
@@ -130,18 +135,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn set_cells_count() {
+    fn set_rows_count() {
         let mut grid = TiltedGrid::default();
-        grid.set_cells_count(7, 15);
-        assert_eq!(grid.horizontal_cells_count(), 7);
-        assert_eq!(grid.vertical_cells_count(), 15);
+        grid.set_rows_count(7);
+        assert_eq!(grid.rows_count(), 7);
     }
     #[test]
-    fn set_incorrect_cells_count() {
+    fn set_incorrect_rows_count() {
         let mut grid = TiltedGrid::default();
-        grid.set_cells_count(0, 0);
-        assert_eq!(grid.horizontal_cells_count(), 1);
-        assert_eq!(grid.vertical_cells_count(), 1);
+        grid.set_rows_count(0);
+        assert_eq!(grid.rows_count(), 1);
+    }
+    #[test]
+    fn set_columns_count() {
+        let mut grid = TiltedGrid::default();
+        grid.set_columns_count(15);
+        assert_eq!(grid.columns_count(), 15);
+    }
+    #[test]
+    fn set_incorrect_columns_count() {
+        let mut grid = TiltedGrid::default();
+        grid.set_columns_count(0);
+        assert_eq!(grid.columns_count(), 1);
     }
     #[test]
     fn tilt() {
