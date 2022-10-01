6556e1aae202a9f2eec974c18cad63283a72986b
OneEyeMaker <oneeyemaker@gmail.com>
2022-10-01 10:19:12 +0300

    Refactored PolygonalStar type accordingly to changes of MosaicShape trait

--- src/mosaic_shape/polygonal_star.rs
+++ src/mosaic_shape/polygonal_star.rs
@@ -51,27 +51,16 @@ impl Default for PolygonalStar {
 }
 
 impl MosaicShape for PolygonalStar {
-    fn set_up_points(
-        &self,
-        image_size: (u32, u32),
-        center: Vector,
-        rotation_angle: f64,
-        scale: f64,
-    ) -> Vec<Vector> {
+    fn set_up_points(&self, image_width: u32, image_height: u32) -> Vec<Vector> {
         let corners_count = self.corners_count as f64;
-        let radius = image_size.0.min(image_size.1) as f64 * 0.5 * scale;
-        let inner_rotation_angle = rotation_angle + consts::PI / corners_count;
+        let radius = image_width.min(image_height) as f64 * 0.5;
+        let inner_rotation_angle = consts::PI / corners_count;
         let inner_radius = radius
             * (consts::PI * (corners_count * 0.5 - 2.0) / corners_count).sin()
             / (consts::FRAC_PI_2 * (corners_count - 2.0) / corners_count).sin();
-        let mut points =
-            helpers::set_up_polygon_points(self.corners_count, radius, center, rotation_angle);
-        let mut inner_points = helpers::set_up_polygon_points(
-            self.corners_count,
-            inner_radius,
-            center,
-            inner_rotation_angle,
-        );
+        let mut points = helpers::set_up_polygon_points(self.corners_count, radius, 0.0);
+        let mut inner_points =
+            helpers::set_up_polygon_points(self.corners_count, inner_radius, inner_rotation_angle);
         points.append(&mut inner_points);
         points
     }
@@ -118,70 +107,67 @@ mod tests {
     #[test]
     fn set_up_points() {
         let star = PolygonalStar::new(4);
-        let points = star.set_up_points(
-            (400, 400),
-            Vector::new(200.0, 200.0),
-            consts::FRAC_PI_4,
-            0.5,
-        );
+        let points = star.set_up_points(400, 400);
         assert_eq!(points.len(), 8);
         for index in 0..4 {
-            let angle = consts::FRAC_PI_2 * index as f64;
+            let angle = consts::FRAC_PI_2 * (index as f64 - 0.5);
             assert_eq!(
                 points[index],
-                Vector::new(200.0 + 100.0 * angle.cos(), 200.0 + 100.0 * angle.sin())
+                Vector::new(200.0 * angle.cos(), 200.0 * angle.sin())
             );
         }
         for index in 4..8 {
-            assert_eq!(points[index], Vector::new(200.0, 200.0));
+            assert_eq!(points[index], Vector::new(0.0, 0.0));
         }
     }
     #[test]
     fn connect_points() {
         let star = PolygonalStar::new(4);
-        let points = star.set_up_points(
-            (400, 400),
-            Vector::new(200.0, 200.0),
-            consts::FRAC_PI_4,
-            0.5,
-        );
+        let points = star.set_up_points(400, 400);
         let segments = star.connect_points(&points);
-        let segment = Segment::from(((100.0, 200.0), (300.0, 200.0)));
+        let segment = Segment::from((
+            (100.0 * 2.0f64.sqrt(), 100.0 * 2.0f64.sqrt()),
+            (-100.0 * 2.0f64.sqrt(), -100.0 * 2.0f64.sqrt()),
+        ));
         assert!(segments.contains(&segment));
-        let segment = Segment::from(((200.0, 100.0), (200.0, 300.0)));
+        let segment = Segment::from((
+            (100.0 * 2.0f64.sqrt(), -100.0 * 2.0f64.sqrt()),
+            (-100.0 * 2.0f64.sqrt(), 100.0 * 2.0f64.sqrt()),
+        ));
         assert!(segments.contains(&segment));
     }
     #[test]
     fn connect_points_of_hexagonal_star() {
         let star = PolygonalStar::new(6);
-        let points = star.set_up_points(
-            (400, 400),
-            Vector::new(200.0, 200.0),
-            consts::FRAC_PI_3,
-            0.5,
-        );
+        let points = star.set_up_points(400, 400);
         let segments = star.connect_points(&points);
-        let segment = Segment::from(((100.0, 200.0), (250.0, 200.0 - 50.0 / 3.0f64.sqrt())));
+        let segment = Segment::from((
+            (100.0, -100.0 * 3.0f64.sqrt()),
+            (100.0, 100.0 * 3.0f64.sqrt()),
+        ));
         assert!(segments.contains(&segment));
-        let segment = Segment::from(((100.0, 200.0), (250.0, 200.0 + 50.0 / 3.0f64.sqrt())));
+        let segment = Segment::from(((-200.0, 0.0), (100.0, -100.0 * 3.0f64.sqrt())));
         assert!(segments.contains(&segment));
-        let segment = Segment::from(((100.0, 200.0), (300.0, 200.0)));
+        let segment = Segment::from((
+            (-100.0, -100.0 * 3.0f64.sqrt()),
+            (100.0, 100.0 * 3.0f64.sqrt()),
+        ));
         assert!(!segments.contains(&segment));
     }
     #[test]
     fn intersect_segments_with_even_corners_count() {
         let star = PolygonalStar::new(8);
-        let points = star.set_up_points((400, 400), Vector::new(200.0, 200.0), 0.0, 0.5);
+        let points = star.set_up_points(400, 400);
         let segments = star.connect_points(&points);
         let intersections = star.intersect_segments(&segments);
-        assert!(!intersections.contains(&Vector::new(200.0, 200.0)));
+        assert!(!intersections.contains(&Vector::new(0.0, 0.0)));
     }
     #[test]
     fn intersect_segments_with_odd_corners_count() {
         let star = PolygonalStar::new(7);
-        let points = star.set_up_points((400, 400), Vector::new(200.0, 200.0), 0.0, 0.5);
+        let points = star.set_up_points(400, 400);
         let segments = star.connect_points(&points);
         let intersections = star.intersect_segments(&segments);
-        assert!(intersections.contains(&Vector::new(200.0, 200.0)));
+        assert!(intersections.contains(&Vector::new(0.0, 0.0)));
     }
 }
